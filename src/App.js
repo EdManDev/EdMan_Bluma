8aeb05efac08304576cdc1856bb8d49f8383ae31
EdManDev <ed.manigat@gmail.com>
2019-03-28 19:11:36 -0400

    update

--- src/App.js
+++ src/App.js
@@ -6,11 +6,35 @@ class App extends Component {
   render() {
     return (
       <section className="section">
-        <div className="class">
-          <h1 className="title">Hello from Bluma</h1>
-          <p className="subtitle">
-            my first website with <strong>Bluma</strong>!
-          </p>
+        <div className="container">
+          <div class="columns">
+            <div class="column">
+              <div className="notification ">
+                First column Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Aliquid, unde atque assumenda, quaerat saepe enim
+                voluptatem, molestias minima et reprehenderit quia iure
+                reiciendis repellat? Nemo a hic aperiam laudantium aliquid.
+              </div>
+            </div>
+            <div className="column">
+              Second column Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Aliquid, unde atque assumenda, quaerat saepe enim
+              voluptatem, molestias minima et reprehenderit quia iure reiciendis
+              repellat? Nemo a hic aperiam laudantium aliquid.
+            </div>
+            <div className="column">
+              Third column Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Aliquid, unde atque assumenda, quaerat saepe enim
+              voluptatem, molestias minima et reprehenderit quia iure reiciendis
+              repellat? Nemo a hic aperiam laudantium aliquid.
+            </div>
+            <div className="column">
+              Fourth column Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Aliquid, unde atque assumenda, quaerat saepe enim
+              voluptatem, molestias minima et reprehenderit quia iure reiciendis
+              repellat? Nemo a hic aperiam laudantium aliquid.
+            </div>
+          </div>
         </div>
       </section>
     );
